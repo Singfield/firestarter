9696ea5ef675b17685cfc7f8d5e3023331ba2640
Singfield <rigaudlouis98@gmail.com>
2020-12-13 17:38:05 +0100

    reverse env config in main branch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDGjAzJ9xJZjemzsdLIBoKPu5u2uSj0CNc",
-    authDomain: "firestarter-2409a.firebaseapp.com",
-    projectId: "firestarter-2409a",
-    storageBucket: "firestarter-2409a.appspot.com",
-    messagingSenderId: "363091281139",
-    appId: "1:363091281139:web:ef5a2392d2cb7f77782386",
-    measurementId: "G-Z7FE5CYPRD"
-  }
 };
