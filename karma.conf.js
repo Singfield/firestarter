bbf8135ffff48b48705e133f7797551c6e73ed2a
Singfield <rigaudlouis98@gmail.com>
2020-12-13 16:42:46 +0100

    modify title

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angularWithValentin'),
+      dir: require('path').join(__dirname, './coverage/firestarter'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularWithValentin'`, () => {
+  it(`should have as title 'firestarter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularWithValentin');
+    expect(app.title).toEqual('firestarter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularWithValentin app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('firestarter app is running!');
   });
 });
